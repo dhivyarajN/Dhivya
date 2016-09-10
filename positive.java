cfbeedfcc25a11e442de1694e676efe0e79db981
dhivyarajN <dhivyaraj141095@gmail.com>
2016-09-10 11:57:58 +0530

    Create positive.java

---
+++ positive.java
@@ -0,0 +1,18 @@
+public class Positive {
+public static void main(Strings[] arg) {
+Scanner try = new Scanner(System.in); 
+System.out.println("enter the number");
+int number= try.nextInt();
+{
+if(num=0)
+{
+System.out.println("the number is zero");
+}
+elseif(num>0){
+System.out.println("the number is positive");
+}
+else{
+System.out.println("the number is negative");
+}
+}
+}
