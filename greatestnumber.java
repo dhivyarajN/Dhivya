1549866c7b6febef9488d2dd5c1b4b3362c8306c
dhivyarajN <dhivyaraj141095@gmail.com>
2016-10-20 14:22:42 +0530

    Create greatestnumber.java

---
+++ greatestnumber.java
@@ -0,0 +1,15 @@
+import.java.util.Scanner;
+public class Greatest Number{
+public static void main(String args[]){
+int a,b,c;
+Scanner s= new Scanner(System.in)
+System.out.println("enter three number);
+a=s.nextInt();
+b=s.nextInt();
+c=s.nextInt();
+if(a>=b&&a>=c)||(b>=a&&b>=c)||(c>=a&&c>=b)
+{
+System.out.println("largest number");
+else
+System.out.println("not");
+}}}
