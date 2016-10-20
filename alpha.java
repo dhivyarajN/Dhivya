54e4ed0306d657347823c08fae85943f9f23fd08
dhivyarajN <dhivyaraj141095@gmail.com>
2016-10-20 12:51:56 +0530

    Create alpha.java

---
+++ alpha.java
@@ -0,0 +1,15 @@
+public class Alpha
+{
+public static void main(string args[]){
+char ch;
+Scanner scan = new Scanner(System.in);
+System.out.println("enter the character");
+ch=scan.next().char();
+if(ch>='a'&& ch<='z')(ch>='A'&&ch<='Z')
+{
+System.out.println("alphabet");
+else
+Syste.out.println("not");
+}
+}
+}
