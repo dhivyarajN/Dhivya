2205b45d53f12cfb68f5ed660bf16098c474cdf5
dhivyarajN <dhivyaraj141095@gmail.com>
2016-10-20 14:07:35 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,8 @@
+public class Leap{
+public static void main(String args[]){
+int year=400;
+if(year%400==0||year%100==0||year%4==0){
+System.out.println("leap year");
+else
+System.out.println("not leap year");
+}}}
