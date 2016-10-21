91b15f88a54e95c4998de3e231679db262bb34ff
dhivyarajN <dhivyaraj141095@gmail.com>
2016-10-21 09:51:09 +0530

    Create amstrong.java

---
+++ amstrong.java
@@ -0,0 +1,16 @@
+public class Amstrong{
+public ststic void main(string args[]){
+int c=0,n,temp,n;
+temp=n;
+while(n>0)
+{
+a=n%10;
+n=n/10;
+c=(a*a*a);
+}
+if( temp==c)
+System.out.println("amstrong");
+else
+System.oyt.println("not");
+}
+}
