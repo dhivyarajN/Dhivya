9163f2ffadd508efa14bc4ec051b07908b812c0f
dhivyarajN <dhivyaraj141095@gmail.com>
2016-10-21 11:13:11 +0530

    Create amint.java

---
+++ amint.java
@@ -0,0 +1,11 @@
+public class Amint{
+public static void main(string args[]){
+Amint returnObj=new Amint();
+int start=100; 
+int end=999;
+for(int i=0;i<end;i++)
+{
+number=i=number;
+returnObj.Amint(number);
+}
+}
